57f5cc10ef225438003a2c7ab13ffcd78c777436
Bart Sky <83052285+bart-sky@users.noreply.github.com>
2022-01-23 13:49:20 -0600

    Create CatMove.cs

---
+++ Scripts/CatMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catMove : MonoBehaviour {
+
+
+    public Rigidbody2D rigidbody;
+
+
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        // Physics, movement, jumping
+        // update is AFTER fixed update
+        // https://docs.unity3d.com/Manual/ExecutionOrder.html
+
+
+
+    }
+
+}
